e78caa9e1fe2587430aa4504c1da80c3ca4474d2
Sergey <sergey.shevchenko04@gmail.com>
2024-01-26 11:52:05 +0300

    trird commit

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function rememberMyFilms() {
         const a = prompt("Один из последних просмотренных фильмов?", ""),
               b = prompt("На сколько оцените его?", "")
 
-        if(a != null && b != null && a.trim != "" && b.trim != "" && a.length < 50 && b.length < 50) {
+        if(a != null && b != null && a.trim() != "" && b.trim() != "" && a.length < 50 && b.length < 50) {
             personalMovieDB.movies[a] = b;
             console.log("done");
         } else {
